cbd4d65d954f21fc51d72579e557add04a0c19b6
ayousri <hemeda2@gmail.com>
2024-09-07 21:31:20 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# CCopier: Code Copying Tool For AI
+
+## Overview
+
+CCopier is an IntelliJ IDEA plugin designed to streamline the process of copying multiple files' content with a single click. It's particularly useful for developers who need to share code snippets with AI tools or other developers quickly and efficiently.
+
+## Features
+
+- Display project structure in an intuitive tree view
+- Copy content of multiple files simultaneously
+- Exclude media files (images, audio, video) from copying operations
+- Refresh file tree on demand
+- Keyboard shortcut support for quick copying
+- Customizable logging for debugging purposes
+
+## Installation
+
+1. Open IntelliJ IDEA
+2. Go to `File > Settings > Plugins` (on macOS, `IntelliJ IDEA > Preferences > Plugins`)
+3. Click on "Marketplace" and search for "CCopier"
+4. Click "Install" and restart IntelliJ IDEA when prompted
+
+
+Thank you for using CCopier! We hope it enhances your coding and AI interaction experience.
---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+
+plugins {
+  id("java")
+  id("org.jetbrains.intellij") version "1.17.4"
+
+
+}
+
+group = "com.mapledoum"
+version = "2.0.1"
+
+repositories {
+  mavenCentral()
+}
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_17
+}
+
+// See https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+intellij {
+  version.set("2023.3.7")
+  plugins.set(listOf("com.intellij.java"))
+
+}
+
+tasks {
+  buildSearchableOptions {
+    enabled = false
+  }
+
+  patchPluginXml {
+    version.set("${project.version}")
+    sinceBuild.set("233")
+    untilBuild.set("242.*")
+  }
+  signPlugin {
+    certificateChain.set(providers.fileContents(layout.projectDirectory.file("sign-code-plugin/chain.crt")).asText)
+    privateKey.set(providers.fileContents(layout.projectDirectory.file("sign-code-plugin/private.pem")).asText)
+    password.set(providers.environmentVariable("PRIVATE_KEY_PASSWORD"))
+  }
+  publishPlugin {
+    token.set(providers.environmentVariable("PUBLISH_TOKEN"))
+  }
+
+}
+
